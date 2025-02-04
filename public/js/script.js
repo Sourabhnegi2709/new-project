c662e63224e3d2c002e3281d5b1682875328ff72
sourabh negi <sourabhnegi557@gmail.com>
2025-02-04 14:33:27 +0530

    update navbar with sidebar

--- public/js/script.js
+++ public/js/script.js
@@ -16,4 +16,29 @@
       false
     );
     });
-  })();
+  })
+
+  ();
+
+ 
+  document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const sidebarToggle = document.getElementById("sidebarToggle");
+    const closeSidebar = document.getElementById("closeSidebar");
+    const overlay = document.getElementById("overlay");
+
+    sidebarToggle.addEventListener("click", function () {
+      sidebar.classList.add("active");
+      overlay.classList.add("active");
+    });
+
+    closeSidebar.addEventListener("click", function () {
+      sidebar.classList.remove("active");
+      overlay.classList.remove("active");
+    });
+
+    overlay.addEventListener("click", function () {
+      sidebar.classList.remove("active");
+      overlay.classList.remove("active");
+    });
+  });
